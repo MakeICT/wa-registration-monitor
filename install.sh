3950f75d234f5065e6095c693787457400ae560c
Christian Kindel <iceman81292@gmail.com>
2017-04-20 22:15:04 -0500

    add installation file to create service

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#############################
+# Enable service
+#############################
+read -p "Install and enable service [Y/n]: " response
+if [ "" = "$response" ] || [ "Y" = "$response" ] || [ "y" = "$response" ]; then
+	f=registration-monitor.service
+	sudo systemctl disable 'registration-monitor' > /dev/null 2>&1
+	echo "
+		[Unit]
+		Description=WildApricot Registration Monitor
+		
+		[Service]
+		ExecStart=`which python3` `pwd`/RegiMon.py
+		WorkingDirectory=`pwd`
+		Restart=always
+		[Install]
+		WantedBy=multi-user.target
+	" | awk '{$1=$1};1' > $f
+
+	sudo systemctl enable `pwd`/$f
+	echo ""
+	echo "    To start the service, use"
+	echo "        sudo systemctl start $f"
+	echo "    or reboot"
+	echo "    To run manually, use"
+	echo "        sudo python3 ./RegiMon.py"
+fi
+echo "Done!"
