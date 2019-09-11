af8a41425a71dca6694591f5e07fd79afcf8e97e
Christian Kindel <iceman81292@gmail.com>
2019-09-11 10:02:18 -0500

    refactor code for syncing active members to Discourse group

--- SyncDiscourseGroups.py
+++ SyncDiscourseGroups.py
@@ -22,105 +22,58 @@ class ChildScript(Script):
             api_username=self.config.get('discourse_api','username'),
             api_key=self.config.get('discourse_api','key'))
 
+        self.member_levels = ['Monthly', '6 months', '12 months', "Scholarship (monthly)", "Scholarship (6 months)", "Scholarship (12 months)"]
+        self.member_group_id = self.discourse_api.group('MakeICT_Members')['group']['id']
 
-    def SyncNewUsers(self):
-        member_levels = ['Monthly', '6 months', '12 months', "Scholarship (monthly)", "Scholarship (6 months)", "Scholarship (12 months)"]
-        member_group_id = self.discourse_api.group('MakeICT_Members')['group']['id']
+    def SyncUser(self, d_user, wa_user):
+        # print(self.discourse_api.user_emails(d_user['user']['username']))
+        print(d_user['user']['username'], ':', d_user['user']['email'], ':', wa_user['Email'])
+        active_member = False
 
-        discourse_users =  self.discourse_api.list_users("new")
-        wa_contacts = self.WA_API.GetAllContacts()
-        print("%d users" % len(discourse_users))
-        # self.discourse_api.add_group_member(member_group_id, users_to_add)
-        for u in discourse_users:
-            # print(self.discourse_api.user_emails(u['user']['username']))
-            u['email'] = self.discourse_api.user_emails(u['username'])['email']
-            print(u['username'],':', u['email'] )
-            wa_contact_info = None
-            for contact in wa_contacts:
-                if contact['Email'] == u['email']:
-                    print("Found WA user for %s" % u['email'])
-                    try:
-                        if contact['MembershipLevel']['Name'] in member_levels:
-                            if contact['Status'] == 'Active':
-                                try:
-                                    print("adding user to group")
-                                    self.discourse_api.add_group_member(member_group_id, u['username'])
-                                except pydiscourse.exceptions.DiscourseClientError:
-                                    print("failed to add user to group")
-                    except KeyError:
-                        print("No membership level found")
-            # if u['user']['email'] in [contact['Email'] for contact in wa_contacts]:
-            #     if 
-            # else:
-            #     print("Did not find WA user for %s" % u['user']['email'])
-            time.sleep(2)
+        print(d_user)
 
-    def SyncAllUsers(self):
-        member_levels = ['Monthly', '6 months', '12 months', "Scholarship (monthly)", "Scholarship (6 months)", "Scholarship (12 months)"]
-        member_group_id = self.discourse_api.group('MakeICT_Members')['group']['id']
+        assert d_user['user']['email'] == wa_user['Email']
 
-        discourse_users =  self.discourse_api.user_list()
-        # for u in discourse_users:
-        #     print(u)
-        # for thing in dir(client):
-        #     print(thing)
-        wa_contacts = self.WA_API.GetAllContacts()
-        for contact in wa_contacts:
-            print(contact['Email'])
-        print("%d users" % len(discourse_users))
+        try:
+            is_member = wa_user['MembershipLevel']['Name'] in self.member_levels
+        except KeyError:
+            print("No membership level found")
+            is_member = False
+        if is_member and wa_user['Status'] == 'Active':
+            print("user is an active member, adding user to group")
+            try:
+                self.discourse_api.add_group_member(self.member_group_id, d_user['user']['username'])
+            except pydiscourse.exceptions.DiscourseClientError:
+                print("failed to add user to group")
+        else:
+            print("user is not an active member, removing user from group")
+            self.discourse_api.delete_group_member(self.member_group_id, d_user['user']['id'])
 
-        for u in discourse_users:
-            # print(self.discourse_api.user_emails(u['user']['username']))
+    def SyncUsers(self, disc_users, wa_users):
+        for u in disc_users:
             u['user']['email'] = self.discourse_api.user_emails(u['user']['username'])['email']
-            print(u['user']['username'],':', u['user']['email'] )
-            active_member = False
-            for contact in wa_contacts:
+
+            for contact in wa_users:
                 if contact['Email'] == u['user']['email']:
                     print("Found WA user for %s" % u['user']['email'])
-                    try:
-                        if contact['MembershipLevel']['Name'] in member_levels:
-                            if contact['Status'] == 'Active':
-                                active_member=True
-                                print("member is active")
-                            else:
-                                print("member is not active")
+                    self.SyncUser(u, contact)
+                    time.sleep(2)
 
-                    except KeyError:
-                        print("No membership level found")
+    def SyncAllUsers(self):
+        discourse_users = self.discourse_api.user_list()
+        wa_contacts = self.WA_API.GetAllContacts()
 
-            if active_member:
-                try:
-                    print("adding user to group")
-                    self.discourse_api.add_group_member(member_group_id, u['user']['username'])
-                except pydiscourse.exceptions.DiscourseClientError:
-                    print("failed to add user to group")
-            else:
-                print("removing user from group")
-                self.discourse_api.delete_group_member(member_group_id, u['user']['id'])
+        self.SyncUsers(discourse_users, wa_contacts)
 
-            # if u['user']['email'] in [contact['Email'] for contact in wa_contacts]:
-            #     if 
-            # else:
-            #     print("Did not find WA user for %s" % u['user']['email'])
-            time.sleep(2)
+    def SyncNewUsers(self):
+        discourse_users =  self.discourse_api.list_users("new")
+        wa_contacts = self.WA_API.GetAllContacts()
+
+        self.SyncUsers(discourse_users, wa_contacts)
 
     def Run(self):
         self.SyncAllUsers()
-        # self.SyncNewUsers()
-        # print("adding all identified users to member group")
-        # cutoff_date=self.WA_API.DateTimeToWADate(datetime.now() - timedelta(days=180))
-        # inactive_contacts = self.WA_API.GetFilteredContacts("'Profile+last+updated'+le+%s+and+'Last+login+date'+le+%s+and+'Membership+level+ID'+eq+813239" % (cutoff_date,cutoff_date))
-        # for contact in inactive_contacts:
-        #     print(contact,'\n')
-        # ids = [contact['Id'] for contact in inactive_contacts]
-        # print(ids)
-        # for id in ids:
-        #     try:
-        #         self.WA_API.UpdateContactField(int(id), "Archived", False)
-        #     except Exception as e:
-        #         print("issue with user id:", id )
-        #         print(e)
-        #         # print(sys.exc_info()[0])
+
 
 if __name__ == "__main__":
     s = ChildScript("Sync Discourse Groups")
