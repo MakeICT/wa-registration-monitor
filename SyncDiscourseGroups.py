d5dadb53733efb5f91f1e1bbf2c4253dfd1611ac
Christian Kindel <iceman81292@gmail.com>
2019-09-11 13:43:27 -0500

    add check for secondary emails in Discourse

--- SyncDiscourseGroups.py
+++ SyncDiscourseGroups.py
@@ -26,19 +26,18 @@ class ChildScript(Script):
                               "Scholarship (monthly)",
                               "Scholarship (6 months)",
                               "Scholarship (12 months)"]
+        self.group_name = 'MakeICT_Members'
         self.member_group_id = \
-            self.discourse_api.group('MakeICT_Members')['group']['id']
+            self.discourse_api.group(self.group_name)['group']['id']
 
     def SyncUser(self, d_user, wa_user):
         # print(self.discourse_api.user_emails(d_user['user']['username']))
         print(d_user['user']['username'], ':',
-              d_user['user']['email'], ':',
+              d_user['user']['emails'], ':',
               wa_user['Email'])
         is_member = False
 
-        print(d_user)
-
-        assert d_user['user']['email'] == wa_user['Email']
+        assert wa_user['Email'] in d_user['user']['emails']
 
         try:
             is_member = wa_user['MembershipLevel']['Name'] in self.member_levels
@@ -47,11 +46,19 @@ class ChildScript(Script):
 
         if is_member and wa_user['Status'] == 'Active':
             print("user is an active member, adding user to group")
+
+            try:
+                if d_user['user']['primary_group_name'] == self.group_name:
+                    print("User is already in group")
+                    return
+            except KeyError:
+                print("User has no primary group")
+
             try:
                 self.discourse_api.add_group_member(
                     self.member_group_id, d_user['user']['username'])
             except DiscourseClientError:
-                print("failed to add user to group")
+                print("Failed to add user to group")
         else:
             print("user is not an active member, removing user from group")
             self.discourse_api.delete_group_member(
@@ -59,14 +66,18 @@ class ChildScript(Script):
 
     def SyncUsers(self, disc_users, wa_users):
         for u in disc_users:
-            u['user']['email'] = \
-                self.discourse_api.user_emails(u['user']['username'])['email']
+            emails = self.discourse_api.user_emails(u['user']['username'])
+            email_list = [emails['email']]
+            email_list += emails['secondary_emails']
+            u['user']['emails'] = email_list
 
             for contact in wa_users:
-                if contact['Email'] == u['user']['email']:
-                    print("Found WA user for %s" % u['user']['email'])
+                if contact['Email'] in u['user']['emails']:
+                    print("Found WA user for %s" % contact['Email'])
                     self.SyncUser(u, contact)
-                    time.sleep(2)
+                    break
+
+            time.sleep(2)
 
     def SyncAllUsers(self):
         discourse_users = self.discourse_api.user_list()
